18427bda692a5c53967e2247842dd38a63250503
Tony Jefferson <tony@mail.rit.edu>
2020-01-28 14:13:15 -0500

    changed server.js

--- src/server.js
+++ src/server.js
@@ -7,11 +7,11 @@ const onRequest = (request, response) => {
   console.log(request.url);
   if (request.url === '/random') {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write(`Hello - ${shortid.generate()}`);
+    response.write(`Hello your short ID is - ${shortid.generate()}`);
     response.end();
   } else {
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write('<html><body><h1>Default Page!!!</h1></body></html>');
+    response.write('<html><body><h1>Default Page:-)</h1></body></html>');
     response.end();
   }
 };
